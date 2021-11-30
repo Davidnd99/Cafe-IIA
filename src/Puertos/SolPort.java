c0a42bd0f61832dbb60fef599f6a7442cdc48db3
David <David@192.168.1.153>
2021-11-30 11:27:49 +0100

    Tareas arregladas

--- src/Puertos/SolPort.java
+++ src/Puertos/SolPort.java
@@ -4,9 +4,13 @@
  */
 package Puertos;
 
+import Conectores.ConectorBC;
+import Conectores.ConectorBF;
 import Tareas.Translator;
+import cafe.Msg;
 import cafe.Slot;
 import java.util.ArrayList;
+import javax.xml.transform.TransformerException;
 import javax.xml.xpath.XPathExpressionException;
 import org.w3c.dom.Document;
 
@@ -18,19 +22,22 @@ public class SolPort {
     
     private Translator t;
     private Slot salida;
+    private ConectorBC BC;
+    private ConectorBF BF;
     
     public SolPort(Translator t, Slot salida){
         this.t = t;
         this.salida = salida;
     }
     
-    public ArrayList<String> Realiza1() throws XPathExpressionException{
+    public ArrayList<String> Realiza1() throws XPathExpressionException, TransformerException{
         //Devuelve la sentencia XPath haciendo uso del traductor
         return t.Realiza();
     }
     
     public void Realiza2(Document doc){
-        salida.Write(doc);
+        Msg m = new Msg(doc);
+        salida.Write(m);
     }
     
 }
